b57f279281568227298d977712cf42f5690a8732
andres95gsi <andres95gsi@gmail.com>
2023-01-25 10:23:03 +0000

    prueba

--- scriptgit.sh
+++ scriptgit.sh
@@ -1,5 +1,5 @@
-find /home/usuario -name "*.txt" -exec tar rvf archivocomprimido.tar {}\;
-
+#!/bin/bash
+find /home/usuario -name "*.txt" -exec tar rvf archivocomprimido.tar {} \;
 git add .
 git commit -m "prueba"
 git push origin main
